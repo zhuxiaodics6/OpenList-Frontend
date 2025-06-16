7b5ed20c608c7b9b36d1950a386678e0a89f8175
Hantong Chen <cxwdyx620@gmail.com>
2025-06-17 01:06:46 +0800

    fix: XSS issue reported

--- src/pages/home/previews/markdown_with_word_wrap.tsx
+++ src/pages/home/previews/markdown_with_word_wrap.tsx
@@ -1,11 +1,18 @@
 import { Markdown, MaybeLoading } from "~/components"
 import { useFetchText } from "~/hooks"
+import { objStore } from "~/store"
+import { ext } from "~/utils"
 
 const MdPreview = () => {
   const [content] = useFetchText()
   return (
     <MaybeLoading loading={content.loading}>
-      <Markdown class="word-wrap" children={content()?.content} toc />
+      <Markdown
+        class="word-wrap"
+        children={content()?.content}
+        ext={ext(objStore.obj.name)}
+        toc
+      />
     </MaybeLoading>
   )
 }
